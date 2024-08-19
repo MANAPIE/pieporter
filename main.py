0035434e4b7316c9f9c140f21faf88d93fa58e6c
MANAPIE <developer@manapie.me>
2024-08-19 23:20:18 +0900

    [FEAT] basic Google search

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello, World!")
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
+
+def google_search(query):
+    GOOGLE_SEARCH_ENGINE_ID = os.environ.get('GOOGLE_SEARCH_ENGINE_ID')
+    GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
+
+    start_page = "1"
+
+    url = f"https://www.googleapis.com/customsearch/v1?key={GOOGLE_API_KEY}&cx={GOOGLE_SEARCH_ENGINE_ID}&q={SEARCH_QUERY}&start={start_page}"
+
+    response = requests.get(url).json()
+
+    return response
+
+SEARCH_QUERY = os.environ.get('SEARCH_QUERY')
+
+print(google_search(SEARCH_QUERY))
