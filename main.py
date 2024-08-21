623cefca0354b432aed879850efad0b8c7249019
MANAPIE <developer@manapie.me>
2024-08-22 02:53:46 +0900

    [FEAT] search more than 1 page

--- main.py
+++ main.py
@@ -10,29 +10,36 @@ def google_search():
     GOOGLE_SEARCH_ENGINE_ID = os.environ.get('GOOGLE_SEARCH_ENGINE_ID')
     GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
     SEARCH_QUERY = os.environ.get('SEARCH_QUERY')
+    ROW_PER_SEARCH = int(os.environ.get('ROW_PER_SEARCH'))
 
     query = SEARCH_QUERY
     query += "-filetype:pdf"
 
-    start_page = "1"
-
+    start_pages = []
     df = pd.DataFrame(columns=['Title', 'Link', 'Description'])
     row_count = 0
 
-    url = f"https://www.googleapis.com/customsearch/v1?key={GOOGLE_API_KEY}&cx={GOOGLE_SEARCH_ENGINE_ID}&q={query}&start={start_page}"
+    for i in range(1, ROW_PER_SEARCH + 10, 10):
+        start_pages.append(i)
 
-    data = requests.get(url).json()
-    search_items = data.get("items")
+    for start_page in start_pages:
+        url = (f"https://www.googleapis.com/customsearch/v1?key={GOOGLE_API_KEY}&cx={GOOGLE_SEARCH_ENGINE_ID}"
+               f"&q={query}&start={start_page}")
 
-    for i, search_item in enumerate(search_items, start=1):
-        if search_item is not None:
-            link = search_item.get("link")
-            title = search_item.get("title")
-            description = search_item.get("snippet")
+        data = requests.get(url).json()
+        search_items = data.get("items")
 
-            df.loc[i] = [title, link, description]
+        for i, search_item in enumerate(search_items, start=1):
+            if search_item is not None:
+                link = search_item.get("link")
+                title = search_item.get("title")
+                description = search_item.get("snippet")
 
-            row_count += 1
+                df.loc[start_page + i] = [title, link, description]
+
+                row_count += 1
+            if row_count >= ROW_PER_SEARCH:
+                break
 
     return df
 
